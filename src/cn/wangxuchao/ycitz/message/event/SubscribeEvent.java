990e247253d2a66c16431d0956c5f013e4b48ffd
王绪超 <gmking@vip.qq.com>
2016-01-14 15:30:51 +0800

    项目框架搭建完成，使用springmvc框架

---
+++ src/cn/wangxuchao/ycitz/message/event/SubscribeEvent.java
@@ -0,0 +1,8 @@
+package cn.wangxuchao.ycitz.message.event;
+
+/**
+ * 关注/取消关注事件
+ */
+public class SubscribeEvent extends BaseEvent {
+
+}
---
+++ src/cn/wangxuchao/ycitz/message/response/Image.java
@@ -0,0 +1,18 @@
+package cn.wangxuchao.ycitz.message.response;
+
+/**
+ * 图片model
+ */
+public class Image {
+
+    // 媒体文件id
+    private String MediaId;
+
+    public String getMediaId() {
+        return MediaId;
+    }
+
+    public void setMediaId(String mediaId) {
+        MediaId = mediaId;
+    }
+}
